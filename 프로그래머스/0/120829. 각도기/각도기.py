5f22b01ea1630725beec02fb218790a51d113b79
KIMEOJIN <88822777+qldirr@users.noreply.github.com>
2024-12-10 19:04:30 +0900

    [level 0] Title: 각도기, Time: 0.00 ms, Memory: 9.94 MB -BaekjoonHub

---
+++ 프로그래머스/0/120829. 각도기/각도기.py
@@ -0,0 +1,11 @@
+def solution(angle):
+    # answer = 0
+    if 0 < angle < 90: return 1
+        # answer = 1
+    elif angle == 90 : return 2
+        # answer = 2
+    elif 90 < angle < 180 : return 3
+        # answer = 3
+    else : return 4
+    #     answer = 4
+    # return answer
