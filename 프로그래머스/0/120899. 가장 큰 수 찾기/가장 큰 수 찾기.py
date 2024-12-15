c441e2bc29d4cf9264ce1979b416c40748dcbb73
KIMEOJIN <88822777+qldirr@users.noreply.github.com>
2024-12-15 20:56:14 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.py
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.py
@@ -6,3 +6,7 @@ def solution(array):
     result.append(max)
     result.append(index)
     return result
+
+# 가장 큰 값 찾기, array.index() 는 해당 값의 인덱스 찾기
+# val = max(array)
+# return [val, array.index(val)]
