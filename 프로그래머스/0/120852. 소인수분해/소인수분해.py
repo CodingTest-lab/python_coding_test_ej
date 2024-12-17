2a3001264c2b04926e81a28e4ab7c7a0e5ef6575
KIMEOJIN <88822777+qldirr@users.noreply.github.com>
2024-12-17 21:20:27 +0900

    [level 0] Title: 소인수분해, Time: 1.75 ms, Memory: 9.82 MB -BaekjoonHub

--- 프로그래머스/0/120852. 소인수분해/소인수분해.py
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.py
@@ -14,3 +14,15 @@ def solution(n):
         else:
             x += 1
     return answer
+
+    # 결과값의 중복을 제거하기 위해 set으로 만들고
+    # 리스트로 변환 후 오름차순 정렬
+    # answer = []
+    # i = 2
+    # while i <= n :
+    #     if n % i == 0 :
+    #         answer.append(i)
+    #         n = n // i
+    #     else:
+    #         i += 1
+    # return sorted(list(set(answer)))
