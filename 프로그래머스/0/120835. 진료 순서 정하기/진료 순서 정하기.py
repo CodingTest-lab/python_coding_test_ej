7e1338575794c1b5e1a9a1f566f3ae8a9aee971f
KIMEOJIN <88822777+qldirr@users.noreply.github.com>
2024-12-17 20:57:40 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.py
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.py
@@ -1,2 +1,17 @@
 def solution(emergency):
+    # 입력 배열을 내림차순으로 정렬
+    # .index(e) : 각 원소의 정렬된 배열에서의 인덱스
+    # 순위는 인덱스 + 1
     return [sorted(emergency, reverse=True).index(e) + 1 for e in emergency]
+
+    # 바깥 for문 : 각 원소 순회
+    # 안쪽 for문 : 해당 원소와 다른 모든 원소 비교
+    # lt는 자신보다 큰 원소의 개수 + 1
+    # arr = []
+    # for i in emergency:
+    #     idx = 1
+    #     for j in emergency:
+    #         if i < j:
+    #             idx += 1
+    #     arr.append(idx)
+    # return arr
