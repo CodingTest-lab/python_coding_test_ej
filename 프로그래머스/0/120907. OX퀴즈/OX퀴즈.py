d1670e701113870afc73757d69093158d124bd15
KIMEOJIN <88822777+qldirr@users.noreply.github.com>
2024-12-18 19:50:39 +0900

    [level 0] Title: OX퀴즈, Time: 0.04 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120907. OX퀴즈/OX퀴즈.py
+++ 프로그래머스/0/120907. OX퀴즈/OX퀴즈.py
@@ -1,16 +1,17 @@
 def solution(quiz):
-    i = -1
     answer = []
-    while 1:
-        if i == len(quiz) -1:
-            break
-        i += 1
-        str = quiz[i].split()
+    for equation in quiz:
+        # 각각의 식을 공백으로 나눔(list로 반환)
+        str = equation.split()
+        
+        # '+' 기호
         if str[1] == '+':
             if int(str[0]) + int(str[2]) == int(str[4]):
                 answer.append('O')
             else:
                 answer.append('X')
+                
+        # '-' 기호
         if str[1] == '-':
             if int(str[0]) - int(str[2]) == int(str[4]):
                 answer.append('O')
