e1ba27ab2d7fc9622bae982fd0210f7553d438bb
KIMEOJIN <88822777+qldirr@users.noreply.github.com>
2024-12-19 20:40:51 +0900

    Update 최댓값과 최솟값.py

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
@@ -1,4 +1,6 @@
 def solution(s):
-    # 공백을 기준으로 
+    # 공백을 기준으로 나누어 리스트로 변환
+    # int 함수를 사용하여 문자열 리스트의 각 요소를 정수로 변환
+    # 배열에서 최솟값과 최댓값을 찾고 문자열로 반환
     arr = list(map(int, s.split(' ')))
     return str(min(arr)) + " " + str(max(arr))
