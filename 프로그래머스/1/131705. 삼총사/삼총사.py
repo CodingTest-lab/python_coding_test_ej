0abc577a061f361b2af03df1284b2744439599f7
KIMEOJIN <88822777+qldirr@users.noreply.github.com>
2024-12-22 20:17:22 +0900

    [level 1] Title: 삼총사, Time: 0.11 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.py
+++ 프로그래머스/1/131705. 삼총사/삼총사.py
@@ -1,6 +1,6 @@
 def solution(number):
     answer = 0
-    # 숫자 3개로 조합할 수 있는 모든 경우의 수를 찾기 위해 3중 for문
+    # 숫자 3개로 조합할 수 있는 모든 경우의 수를 찾기 위해 3중 for문, 중복 제거
     for i in range(len(number) - 2):
         for j in range(i + 1, len(number) - 1):
             for k in range(j + 1, len(number)):
