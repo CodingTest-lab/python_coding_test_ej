ed6a9ab35177bea499703d058ff4ddeab9899402
KIMEOJIN <88822777+qldirr@users.noreply.github.com>
2024-12-22 21:39:56 +0900

    Update 피보나치 수.py

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
@@ -7,3 +7,16 @@ def solution(n):
         number.append(number[i-1] + number[i-2])  # 앞 + 뒤한 합을 배열 뒤에 추가
     
     return number[n]%1234567
+
+    # 위는 배열을 사용한 방법이고 아래는 그냥
+    # if n == 0:
+    #     return 0
+    # elif n == 1: 
+    #     return 1 
+    # a, b = 0, 1
+    # for _ in range(2, n + 1):
+    #     a, b = b, a+b 
+    #     # temp = b 
+    #     # b = temp + a 
+    #     # a = temp 
+    # return b % 1234567
