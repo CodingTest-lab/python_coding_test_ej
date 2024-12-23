9b3cc5992bd80cfeacf4c17b2ce600ba85bf90a0
KIMEOJIN <88822777+qldirr@users.noreply.github.com>
2024-12-23 19:15:04 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 0.27 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.py
@@ -0,0 +1,11 @@
+def solution(data, ext, val_ext, sort_by):
+    # data에서 ext값이 val_ext보다 작은 데이터만 뽑은 후 sort_by에 해당하는 값 기준으로 오름차순 정렬
+    criteria = {
+        'code':0, 'date':1, 'maximum':2, 'remain':3
+    }
+    answer = []
+    for i in data:
+        if i[criteria[ext]] < val_ext:
+            answer.append(i)
+    sorted_answer = sorted(answer, key=lambda x: x[criteria[sort_by]])
+    return sorted_answer
