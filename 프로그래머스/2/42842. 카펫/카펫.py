966ac54ebf5037db8d10392fc304c9f0384db280
KIMEOJIN <88822777+qldirr@users.noreply.github.com>
2024-12-29 22:17:09 +0900

    Update 카펫.py

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -6,12 +6,14 @@ def solution(brown, yellow):
     # 전체 카펫 크기는 n+2, m+2
     
     # yellow 가로 최소 길이 1
-    range_n = int((brown-4)/2)
     # n의 최대값은 (brown-4)/2
+    range_n = int((brown-4)/2)
+    
     for n in range(range_n):
+        # range_n - n = m
         if n * (range_n - n) == yellow:
-            answer.append(n+2)
-            answer.append(range_n - n + 2)
+            answer.append(n+2)   # n+2
+            answer.append(range_n - n + 2)  # m+2
             break
     answer.sort(reverse=True)
     return answer
