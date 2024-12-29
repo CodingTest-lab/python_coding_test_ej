d3e65de8083b470c750f712465266f18194e0ccb
KIMEOJIN <88822777+qldirr@users.noreply.github.com>
2024-12-29 21:25:43 +0900

    Update 숫자 문자열과 영단어.py

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -1,22 +1,23 @@
 def solution(s):
     eng_num = {
-        "zero": 0,
-        "one": 1,
-        "two": 2,
-        "three": 3,
-        "four": 4,
-        "five": 5,
-        "six": 6,
-        "seven": 7,
-        "eight": 8,
-        "nine": 9,
+        "zero": "0",
+        "one": "1",
+        "two": "2",
+        "three": "3",
+        "four": "4",
+        "five": "5",
+        "six": "6",
+        "seven": "7",
+        "eight": "8",
+        "nine": "9",
     }
     
     # 각 단어를 숫자로 대체
-    for eng in eng_num:
-        s = s.replace(eng, str(eng_num[eng]))
+    for eng, num in eng_num.items():
+        if eng in s:
+            s = s.replace(eng, num)
     
     return int(s)
-# 딕셔너리 .items() 메서드 : 딕셔너리의 키-값 쌍을 튜플 형태로 반환
+    # 딕셔너리 .items() 메서드 : 딕셔너리의 키-값 쌍을 튜플 형태로 반환
     # eng_num은 key에만 접근 가능
     # eng_num.items()를 사용해야 key/value 둘다 접근 가능
