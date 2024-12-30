a1c0125da54acd771484b7b7700e0a8e3d80658c
KIMEOJIN <88822777+qldirr@users.noreply.github.com>
2024-12-30 14:59:11 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 0.04 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.py
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.py
@@ -1,12 +1,10 @@
-# 입력받은 시간을 분/초로 나누는 함수
+# 입력받은 시간을 초로 나타내는 함수
 def time_to_sec(time):
     mm, ss = map(int, time.split(':'))
     return 60 * mm + ss
 
 def solution(video_len, pos, op_start, op_end, commands):
     
-    # pos < video_len
-    # 'prev', 'next'를 했을때 비디오의 가장 처음과 끝을 넘을수는 없음
     len_sec = time_to_sec(video_len)
     pos_sec = time_to_sec(pos)
     op_s_sec = time_to_sec(op_start)
@@ -18,19 +16,13 @@ def solution(video_len, pos, op_start, op_end, commands):
         
     # 명령어에 따라 구간 이동
     for command in commands:
-        # 10초 전 이동
+        # 10초 전 이동 -> video_len과 10초 전 중 큰거
         if command == 'prev':
-            pos_sec -= 10
-        # 10초 후 이동
+            pos_sec = max(0, pos_sec - 10)
+        # 10초 후 이동 -> video_len과 10초 후 중 작은거
         else:
-            pos_sec += 10
+            pos_sec = min(len_sec, pos_sec + 10)
             
-        # pos가 0보다 작으면 pos는 00:00으로 이동
-        if pos_sec < 0:
-            pos_sec = 0
-        # pos가 video_len보다 크면 pos는 영상 마지막으로 이동
-        elif pos_sec > len_sec:
-            pos_sec = len_sec
         # op_start <= pos <= op_end 라면 pos는 op_end로 이동
         if op_s_sec <= pos_sec <= op_e_sec:
             pos_sec = op_e_sec
